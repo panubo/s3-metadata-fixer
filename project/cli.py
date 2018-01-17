4345e53799bcc44083f0d6e2d72032e90ebd1423
Tim Robinson <tim@panubo.com>
2018-01-17 15:46:24 +1100

    Improve handling of metadata items and skip commit if details don't change

--- project/cli.py
+++ project/cli.py
@@ -13,12 +13,19 @@ BUCKET_NAME_REGEX = '^[a-zA-Z0-9.\-_]{1,255}$'
 
 UNKNOWN_MIMETYPE = 'application/octet-stream'
 
+KEY_MAP = {'cache_control': 'CacheControl',
+    'content_disposition': 'ContentDisposition',
+    'content_encoding': 'ContentEncoding',
+    'content_language': 'ContentLanguage',
+    'content_type': 'ContentType',
+    'metadata': 'Metadata'}
+
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('bucket')
 @click.argument('prefix', required=False, default="")
-@click.option('--update-content-type', is_flag=True, help="Update objects Content-Type based on file extension")
+@click.option('--update-content-type', is_flag=True, help="Update objects Content-Type and Content-Encoding based on file extension")
 @click.option('--update-cache-control', help="Update objects Cache-Control, eg. 'max-age=86400'")
 @click.option('--dry-run', is_flag=True)
 @click.option('--debug/--no-debug', default=False)
@@ -27,7 +34,7 @@ def main(**kwargs):
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(kwargs["bucket"])
-    for obj in bucket.objects.filter(Prefix=kwargs["prefix"]):
+    for obj in bucket.objects.filter(Delimiter='', Prefix=kwargs["prefix"]):
         full_obj = obj.Object()
         click.secho("%s Cache-Control: %s, Content-Type: %s, " % (obj.key, full_obj.cache_control, full_obj.content_type), fg='green')
 
@@ -38,19 +45,7 @@ def main(**kwargs):
         new = {"CopySource": {'Bucket': kwargs["bucket"], 'Key': obj.key},
             "MetadataDirective": "REPLACE"}
 
-        if full_obj.cache_control:
-            new['CacheControl'] = full_obj.cache_control
-        if full_obj.content_disposition:
-            new['ContentDisposition'] = full_obj.content_disposition
-        if full_obj.content_encoding:
-            new['ContentEncoding'] = full_obj.content_encoding
-        if full_obj.content_language:
-            new['ContentLanguage'] = full_obj.content_language
-        if full_obj.content_type:
-            new['ContentType'] = full_obj.content_type
-        if full_obj.metadata:
-            new['Metadata'] = full_obj.metadata
-
+        update = False
 
         if kwargs['update_cache_control']:
             new['CacheControl'] = kwargs['update_cache_control']
@@ -59,10 +54,24 @@ def main(**kwargs):
             if new_encoding != None:
                 new['ContentEncoding'] = new_encoding
 
-        click.secho("%s Cache-Control: %s, Content-Type: %s, " % (obj.key, dict.get(new, 'CacheControl', "None"), dict.get(new, 'ContentType', "None")), fg='yellow')
+        # Get current values and add them to the new request
+        for key in KEY_MAP.keys():
+            # If a new key has been added
+            if dict.get(new, KEY_MAP[key]) and not getattr(full_obj, key):
+                update = True
+            # If a new key doesn't match an exiting key
+            if dict.get(new, KEY_MAP[key]) and dict.get(new, KEY_MAP[key]) != getattr(full_obj, key):
+                update = True
+            # Else if an old key exists
+            elif getattr(full_obj, key):
+                new[KEY_MAP[key]] = getattr(full_obj, key)
 
+        # Print the new request
         if kwargs['debug']: click.secho(pformat(new))
-        if not kwargs['dry_run']: full_obj.copy_from(**new)
+        # Only do copy_from is the object is getting an update
+        if update:
+            click.secho("%s Cache-Control: %s, Content-Type: %s, " % (obj.key, dict.get(new, 'CacheControl', "None"), dict.get(new, 'ContentType', "None")), fg='yellow')
+            if not kwargs['dry_run']: full_obj.copy_from(**new)
 
 if __name__ == '__main__':
     main()
